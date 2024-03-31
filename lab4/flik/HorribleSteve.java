398316e99da4acd972df4a1921aea8885a54faf0
hashjenny <huangxudong717@outlook.com>
2024-03-31 18:56:59 +0800

    lab4: fix style

--- lab4/flik/HorribleSteve.java
+++ lab4/flik/HorribleSteve.java
@@ -1,7 +1,7 @@
 package flik;
 
 public class HorribleSteve {
-    public static void main(String [] args) throws Exception {
+    public static void main(String[] args) throws Exception {
         int i = 0;
         int j = 0;
         while (i < 500) {
@@ -9,8 +9,8 @@ public class HorribleSteve {
                 throw new Exception(
                         String.format("i:%d not same as j:%d ??", i, j));
             }
-            i ++;
-            j ++;
+            i++;
+            j++;
         }
         System.out.println("i is " + i);
     }
