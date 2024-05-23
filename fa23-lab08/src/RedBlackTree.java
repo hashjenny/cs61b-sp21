dca99b6bc8d615faca9807b87daddd9cbf2f6379
hashjenny <huangxudong717@outlook.com>
2024-05-24 00:26:51 +0800

    fa23-lab8: try to pass the test

--- fa23-lab08/src/RedBlackTree.java
+++ fa23-lab08/src/RedBlackTree.java
@@ -62,23 +62,95 @@ public class RedBlackTree<T extends Comparable<T>> {
     /* Flips the color of node and its children. Assume that NODE has both left
        and right children. */
     void flipColors(RBTreeNode<T> node) {
-        // TODO: YOUR CODE HERE
+        if (node == null) {
+            return;
+        } else if (node.left == null || node.right == null) {
+            return;
+        } else if (node.left.isBlack || node.right.isBlack) {
+            return;
+        }
+        node.isBlack = false;
+        node.left.isBlack = true;
+        node.right.isBlack = true;
+        if (node == this.root) {
+            return;
+        }
+        flipColors(findParent(this.root, null, node.item).parent);
     }
 
     /* Rotates the given node to the right. Returns the new root node of
        this subtree. For this implementation, make sure to swap the colors
        of the new root and the old root!*/
     RBTreeNode<T> rotateRight(RBTreeNode<T> node) {
-        // TODO: YOUR CODE HERE
-        return null;
+        if (node == null || node.left == null) {
+            return null;
+        }
+
+//      see  https://fa23.datastructur.es/materials/lab/lab08/#llrb-insertion-summary
+        var isRoot = node == this.root;
+        var leftNode = node.left;
+        var leftPart = node.left.left;
+        var midPart = node.left.right;
+        var rightPart = node.right;
+        var parentInfo = findParent(this.root, null, node.item);
+
+//      rotateToRightNode => the root node rotate to right
+        var rotateToRightNode = new RBTreeNode<>(leftNode.isBlack, node.item);
+        rotateToRightNode.left = midPart;
+        rotateToRightNode.right = rightPart;
+
+//      node => the left of the root node rotate to right, become a new root node
+        node = new RBTreeNode<>(node.isBlack, leftNode.item);
+        node.left = leftPart;
+        node.right = rotateToRightNode;
+        if (isRoot) {
+            this.root = node;
+        } else if (this.root != null){
+            var parent = parentInfo.parent;
+            if (parentInfo.side <0) {
+                parent.left = node;
+            } else if (parentInfo.side > 0) {
+                parent.right = node;
+            }
+        }
+        return node;
     }
 
     /* Rotates the given node to the left. Returns the new root node of
        this subtree. For this implementation, make sure to swap the colors
        of the new root and the old root! */
     RBTreeNode<T> rotateLeft(RBTreeNode<T> node) {
-        // TODO: YOUR CODE HERE
-        return null;
+        if (node == null || node.right == null) {
+            return null;
+        }
+
+//      see  https://fa23.datastructur.es/materials/lab/lab08/#llrb-insertion-summary
+        var isRoot = node == this.root;
+        var rightNode = node.right;
+        var leftPart = node.left;
+        var midPart = node.right.left;
+        var rightPart = node.right.right;
+        var parentInfo = findParent(this.root, null, node.item);
+
+        var rotateToLeftNode = new RBTreeNode<>(rightNode.isBlack, node.item);
+        rotateToLeftNode.left = leftPart;
+        rotateToLeftNode.right = midPart;
+
+        node = new RBTreeNode<>(node.isBlack, rightNode.item);
+        node.left = rotateToLeftNode;
+        node.right = rightPart;
+
+        if (isRoot) {
+            this.root = node;
+        } else if (this.root != null){
+            var parent = parentInfo.parent;
+            if (parentInfo.side <0) {
+                parent.left = node;
+            } else if (parentInfo.side > 0) {
+                parent.right = node;
+            }
+        }
+        return node;
     }
 
     public void insert(T item) {
@@ -105,15 +177,67 @@ public class RedBlackTree<T extends Comparable<T>> {
             node.right = insert(node.right, item);
         }
 
-        // TODO: YOUR CODE HERE
+//        node is black
+        if (isBlack(node)) {
 
-        // Rotate left operation
+            if (isBlack(node.left) && isRed(node.right)) {
+                rotateLeft(node);
+            }
+            if (isRed(node.left) && isRed(node.right)) {
+                flipColors(node);
+            }
+        } else {
+            //        node is red
+            if (isRed(node.left)) {
+                var parent = findParent(root, null, node.item).parent;
+                var newNode = rotateRight(parent);
+                flipColors(newNode);
+            }
 
-        // Rotate right operation
+            if (isRed(node.right)) {
+                var newNode = rotateLeft(node);
+                newNode = rotateRight(findParent(this.root, null, newNode.item).parent);
+                flipColors(newNode);
+            }
+        }
 
-        // Color flip
+        return this.root;
+//        if (comp < 0)
+//            return node.left;
+//        else
+//            return node.right;//fix this return statement
+    }
 
-        return null; //fix this return statement
+    private ParentInfo findParent(RBTreeNode<T> currentNode, ParentInfo parentInfo, T target) {
+        if (currentNode == null) {
+            return null;
+        }
+        if (this.root.item.compareTo(target) == 0) {
+            return null;
+        }
+        var result = currentNode.item.compareTo(target);
+        if (result == 0) {
+            return parentInfo;
+        } else if (result > 0) {
+            return findParent(currentNode.left,
+                    new ParentInfo(currentNode, -1), target);
+        } else {
+            return findParent(currentNode.right,
+                    new ParentInfo(currentNode, 1), target);
+        }
+    }
+
+    class ParentInfo {
+        RBTreeNode<T> parent;
+        int side;
+        ParentInfo(RBTreeNode<T> parent, int side) {
+            this.parent = parent;
+            this.side = side;
+        }
+    }
+
+    private boolean isBlack(RBTreeNode<T> node) {
+        return !isRed(node);
     }
 
     /* Returns whether the given node is red. Null nodes (children of leaf
